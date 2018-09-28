2b9f45fdef2cf7a315e64e6adeecb8828e5da63a
Abhicoding <shinganeabhishek@gmail.com>
2018-09-29 02:07:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const app = require('./server')
+
+const port = process.env.PORT || 3000
+
+const server = http.createServer(app)
+
+server.listen(port, () => console.log(`App listening on PORT ${port}`))
