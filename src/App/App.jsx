82cc00f38835a19e0e4d7fd4758f66127659381c
Abhicoding <shinganeabhishek@gmail.com>
2018-09-29 02:49:05 +0530

    Getting a story on app component

---
+++ src/App/App.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      story : {
+          "by" : "",
+          "descendants" : 0,
+          "id" : 0,
+          "kids" : [],
+          "score" : 0,
+          "time" : 0,
+          "title" : "",
+          "type" : "",
+          "url" : ""
+      }
+    }
+  }
+  componentWillMount () {
+    fetch(`https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        this.setState({
+          story: data
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Well to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
