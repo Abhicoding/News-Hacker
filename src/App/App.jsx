1606432a0118966761ea7ab10be241b78316ead2
Abhicoding <shinganeabhishek@gmail.com>
2018-09-30 13:12:04 +0530

    getting stories now

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -8,12 +8,16 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
+      showcount: 10,
       newstories : [],
       topstories: [],
       beststories: [],
-      'newstoriesID': [],
-      'topstoriesID': [],
-      'beststoriesID': [],
+      newstoriesID: [],
+      topstoriesID: [],
+      beststoriesID: [],
+      newstoriespage: 1,
+      topstoriespage: 1,
+      beststoriespage: 1,
       story : {
           "by" : "",
           "descendants" : 0,
@@ -28,6 +32,7 @@ class App extends Component {
     }
     // this.getStoryIds = this.getStoryIds.bind(this)
   }
+
   componentWillMount () {
     ['topstoriesID', 'beststoriesID', 'newstoriesID'].forEach(tab => {
       this.getStoryIds(tab)
@@ -40,11 +45,29 @@ class App extends Component {
       .then(data => {
         var update = {}
         update[tab] = data
-        this.setState({...update})
+        this.setState({...update}, () => this.getStories(tab))
       })
   }
 
-  
+  async getStories (tabName) {
+    var page = this.state[`${tabName.slice(0, -2)}page`]
+    var arr = this.state[tabName]
+      .slice(page-1, page * this.state.showcount)
+      .map(id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?`))
+    var promiseArr = await Promise.all(arr.map(p => p.catch(e => e)))
+    var dataArr = await Promise.all(promiseArr.map(x => x.json()))
+    this.setStories(tabName.slice(0, -2), dataArr)
+  }
+
+  setStories (tabName, storyArr) {
+    var prevArr = this.state[tabName]
+    var newArr = [...prevArr, ...storyArr]
+    var update = {[tabName]: newArr}
+    //console.log(update)
+    this.setState({
+      ...update
+    })
+  }
 
   render() {
     return (
@@ -52,7 +75,8 @@ class App extends Component {
         <div className="App">
           <Route path='/'  render={props => <Header {...props} 
             getStoryIds={this.getStoryIds}/>} />
-          <Route path="/" render={props => <Pagecontent {...props} data={this.state}/>} />
+          <Route path="/" render={props => <Pagecontent {...props} 
+            data={this.state}/>} />
         </div>
       </BrowserRouter>
     );
